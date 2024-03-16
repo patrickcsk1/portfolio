7aaa82330ad39264208a8e52676ef47ada947cd4
patrick.figueroa <patrick.figueroa@patagoniansys.com>
2024-03-16 18:42:26 -0500

    fix: remove unneeded flex

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -12,7 +12,7 @@ import { scrollToSection } from "@/utils/scrolls";
 
 function HeroSection() {
   return (
-    <section className="relative flex flex-col items-center justify-between py-4 lg:py-12 bg-[url('/hero.svg')] bg-cover bg-center bg-no-repeat">
+    <section className="relative py-4 lg:py-12 bg-[url('/hero.svg')] bg-cover bg-center bg-no-repeat">
       <div className="grid grid-cols-1 items-start lg:grid-cols-2 lg:gap-12 gap-y-8">
         <div className="order-2 lg:order-1 flex flex-col items-start justify-center p-2 pb-20 md:pb-10 lg:pt-10">
           <h1 className="text-3xl font-bold leading-10 text-white md:font-extrabold lg:text-[2.6rem] lg:leading-[3.5rem]">
